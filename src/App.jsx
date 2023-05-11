9b4007e56b15891e0d6f7f966fb554f754dcd378
Adam Emery <aqemery@gmail.com>
2023-05-11 16:09:24 -0400

    adding example

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import './App.css'
+
+import { Pyodide } from './pyodide'
+
+function App() {
+  const [pyprompt, setPyprompt] = useState('print("hello world!")')
+  const [pyoutput, setPyoutput] = useState(null)
+  const pyodide = Pyodide.getInstance()
+
+  return (
+    <>
+      <h1>Vite + React + Pyodide</h1>
+      <div>
+        <textarea style={{width: '100%', height: '200px', fontFamily: 'monospace', fontSize: '1rem'}} value={pyprompt}
+        onChange={(e) => {
+          setPyprompt(e.target.value)
+          console.log(e.target.value)
+        }}>
+          
+          </textarea>
+
+          <button onClick={() => {
+            pyodide.setOutput((text) => {
+              setPyoutput(text)
+            })
+            console.log('clicked', pyprompt)
+            pyodide.run(pyprompt)            
+          }}>Run</button>
+
+          <p>Ouput:</p>
+          <code>{pyoutput}</code>  
+      </div>
+    </>
+  )
+}
+
+export default App
+
+
---
+++ src/pyodide.js
@@ -0,0 +1,52 @@
+import { loadPyodide } from "pyodide";
+
+export var Pyodide = (function () {
+  var instance;
+  function createInstance() {
+    var object = new PythonRunner();
+    return object;
+  }
+  return {
+    getInstance: function () {
+      if (!instance) {
+        instance = createInstance();
+      }
+      return instance;
+    },
+  };
+})();
+
+class PythonRunner {
+  constructor() {
+    this._output = console.log;
+    this._pyodide = null;
+    loadPyodide({
+      indexURL: "https://cdn.jsdelivr.net/pyodide/v0.23.2/full",
+      stderr: (text) => {
+        this._output(text);
+      },
+      stdout: (text) => {
+        this._output(text);
+      },
+    }).then((result) => {
+      this._pyodide = result;
+
+      console.log(
+        this._pyodide.runPython(`
+            import sys
+            sys.version
+        `)
+      );
+
+      this._pyodide.runPython('print("Hello from Python!")');
+    });
+  }
+  setOutput(output) {
+    this._output = output;
+  }
+  run(code) {
+    if (this._pyodide) {
+      this._pyodide.runPython(code);
+    }
+  }
+}
