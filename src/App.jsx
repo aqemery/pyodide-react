0320cd22d6f564cf34e2f64adf7bc967bf225336
Adam Emery <aqemery@gmail.com>
2023-05-11 17:17:04 -0400

    formatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,48 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-import { Pyodide } from './pyodide'
+import { Pyodide } from "./pyodide";
 
 function App() {
-  const [pyprompt, setPyprompt] = useState('print("hello world!")')
-  const [pyoutput, setPyoutput] = useState(null)
-  const pyodide = Pyodide.getInstance()
+  const [pyprompt, setPyprompt] = useState('print("hello world!")');
+  const [pyoutput, setPyoutput] = useState(null);
+  const pyodide = Pyodide.getInstance();
 
   return (
     <>
       <h1>Vite + React + Pyodide</h1>
       <div>
-        <textarea style={{width: '100%', height: '200px', fontFamily: 'monospace', fontSize: '1rem'}} value={pyprompt}
-        onChange={(e) => {
-          setPyprompt(e.target.value)
-          console.log(e.target.value)
-        }}>
-          
-          </textarea>
+        <textarea
+          style={{
+            width: "100%",
+            height: "200px",
+            fontFamily: "monospace",
+            fontSize: "1rem",
+          }}
+          value={pyprompt}
+          onChange={(e) => {
+            setPyprompt(e.target.value);
+            console.log(e.target.value);
+          }}
+        ></textarea>
 
-          <button onClick={() => {
+        <button
+          onClick={() => {
             pyodide.setOutput((text) => {
-              setPyoutput(text)
-            })
-            console.log('clicked', pyprompt)
-            pyodide.run(pyprompt)            
-          }}>Run</button>
+              setPyoutput(text);
+            });
+            console.log("clicked", pyprompt);
+            pyodide.run(pyprompt);
+          }}
+        >
+          Run
+        </button>
 
-          <p>Ouput:</p>
-          <code>{pyoutput}</code>  
+        <p>Ouput:</p>
+        <code>{pyoutput}</code>
       </div>
     </>
-  )
+  );
 }
 
-export default App
-
-
+export default App;
